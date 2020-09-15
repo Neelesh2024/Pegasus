defd61ae3e2decb77f3fec7fea321c24faac6548
Neelesh2024 <71237537+Neelesh2024@users.noreply.github.com>
2020-09-15 11:46:49 -0400

    Update DemoTableViewCell.swift

--- DemoTableViewCell.swift
+++ DemoTableViewCell.swift
@@ -13,7 +13,7 @@ class DemoTableViewCell: UITableViewCell {
     @IBOutlet weak var articleImage: UIImageView!
     
     @IBOutlet weak var titleLabel: UILabel!
-    
+    print("monkey")
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
